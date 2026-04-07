b336187ed52f2b7e2f10a548b279bc36fbe28df5
SYR-hs <ssolar8750@hs.ac.kr>
2026-04-07 16:48:34 +0900

    main6-2.py

--- main6-2.py
+++ main6-2.py
@@ -6,12 +6,13 @@ gas = DigitalInputDevice(17)
 
 try:
     while True:
-        if gas.value == 0:    # ← 0 = 가스 감지 (LOW)
+        if gas.value == 0:       # 0 = 가스 감지 (LOW) -> 평상시에 1(High)을 유지하다가 가스를 감지하면 0(Low) 신호를 내보내는 Active Low 방식으로 작동.
+                                 # 0일 때 "가스 감지됨"이라고 판단.
             print("가스 감지됨")
-            bz.on()
-        else:                 # ← 1 = 정상 (HIGH)
+            bz.on()              # 가스 감지시 부저 작동 on
+        else:                    # 1 = 정상 (HIGH)
             print("정상")
-            bz.off()
+            bz.off()             # 가스 미감지시 부저 작동 off
 
         time.sleep(0.2)
 
